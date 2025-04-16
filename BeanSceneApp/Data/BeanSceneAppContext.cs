e85016c17d71d90268274fe13b267a59f92a4d0c
jeremyShum37 <jeremy.shum2@studytafensw.edu.au>
2025-04-16 23:04:29 +1000

    Add project files.

---
+++ BeanSceneApp/Data/BeanSceneAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BeanSceneApp.Models;
+
+namespace BeanSceneApp.Data
+{
+    public class BeanSceneAppContext : DbContext
+    {
+        public BeanSceneAppContext (DbContextOptions<BeanSceneAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BeanSceneApp.Models.User> User { get; set; } = default!;
+    }
+}
---
+++ BeanSceneApp/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BeanSceneApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set;}
+
+        [Required]
+        public string FirstName { get; set; } = "";
+
+        [Required]
+        public string LastName { get; set; } = "";
+
+        [Required]
+        public string Email { get; set; } = "";
+
+        public string? Phone { get; set; }
+
+        [Required]
+        public string Password { get; set; } = "";
+
+        public string? Image { get; set; }
+
+        [Required]
+        public string Role { get; set; } = "";
+    }
+}
